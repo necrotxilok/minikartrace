a9041ab3f84569b2af7855ce4a2b581824ac87ef
necro_txilok <necro_txilok@hotmail.com>
2020-02-22 10:35:39 +0100

    Startup Project

---
+++ Assets/NTK - LabCave/Scripts/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+	public GameObject bestLapTimeTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float bestLapTime = PlayerPrefs.GetFloat("BestLapTime");
+
+        if (!bestLapTimeTextObject) {
+        	Debug.Log("Please, assign a TextMeshPro Object to display Player Lap Time");
+        	return;
+        }
+        TextMeshPro bestLapTimeText = bestLapTimeTextObject.GetComponent<TextMeshPro>();
+
+        if (bestLapTime == 0.0f) {
+    		bestLapTimeTextObject.SetActive(false);
+    		return;
+        }
+
+		bestLapTimeTextObject.SetActive(true);
+		if (bestLapTime < 5.0f) {
+			bestLapTimeText.text = "Hey, are you cheating the game?";
+			return;
+		}
+
+		bestLapTimeText.text = "Best Lap Time: " + bestLapTime + "s";
+    }
+
+
+    public void StartRace() 
+    {
+    	SceneManager.LoadScene("GamePlay");
+    }
+
+    public void ExitGame() 
+    {
+    	Application.Quit();
+    }
+
+}
---
+++ Assets/NTK - LabCave/Scripts/MenuButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MenuButton : MonoBehaviour
+{
+
+	public float startScale = 1.0f;
+	public float endScale = 2.0f; 
+	public float duration = 1.0f;
+
+	public UnityEvent onAnimationEnd;
+
+	private bool animate;
+    private float startTime;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animate) {
+			float timeCovered = (Time.time - startTime) / duration;
+			var nextSize  = Vector3.Lerp(Vector3.one * startScale, Vector3.one * endScale, timeCovered);
+
+			transform.localScale = nextSize;
+
+			if (Vector3.Distance(transform.localScale, Vector3.one * endScale) < 0.05) {
+				onAnimationEnd.Invoke();
+				StopAnimation();
+			}
+        }
+    }
+
+    public void StartAnimation()
+    {
+    	startTime = Time.time;
+    	animate = true;
+    }
+
+    public void StopAnimation()
+    {
+    	animate = false;
+    }
+}
